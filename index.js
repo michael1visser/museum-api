d43dd617bcfc69f60e8078aaedd3366ddffbbf65
mike_visser <mpviss@gmail.com>
2020-07-29 16:30:35 -0400

    confirmed artist route functionality

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = require('express')()
 
 app.use(bodyParser.json())
 app.use(require('./routes/routesIndex'))
-//app.use(require('./routes/artistRoutes'))
 
 app.listen(4000, () =>{
     console.log("Beuler...?")
